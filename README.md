3fa931820055580ef43310ffc02654a6f0c13fde
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-06-15 13:53:18 -0400

    Changed dependencies
    Moved from termion to crossterm
    
    Use coolor for color handling

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # mrs-matrix
 
-A multiplatform clone of cmatrix, written in Rust.
+A **m**ultiplatform clone of cmatrix, written in **R**u**s**t.
 
-Unlike the original cmatrix, supports both Windows and Linux. To this end, uses the [termion](https://github.com/redox-os/termion) library for multiplatform terminal control.
+Unlike the original cmatrix, supports both Windows and Linux. To this end, uses the [crossterm](https://github.com/crossterm-rs/crossterm) library for multiplatform terminal control.<br>
+Also uses [coolor](https://github.com/Canop/coolor) for color management.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use termion::color;
+use std::io::{stdout};
+use crossterm::{self, ExecutableCommand, style::{Stylize, PrintStyledContent}, terminal, cursor};
+use coolor::{Color,Hsl};
 
-fn main() {
-    println!("{}{}{}",
-        color::Fg(color::Green),
-        "Hello, world!", 
-        color::Fg(color::Reset));
+fn main() -> crossterm::Result<()> {
+
+    let mut out = stdout();
+
+    let (term_cols, _term_rows) = terminal::size()?;
+
+    //get middle column index by dividing the term_cols by 2 (rounding up), subtracting 1 for zero based indexing
+    let mid_col =((term_cols + 1) / 2) - 1;
+
+    println!("writing to column index {} within a terminal {} columns wide", mid_col, term_cols);
+
+    out.execute(cursor::MoveToColumn(mid_col))?
+        .execute(PrintStyledContent("H".with(
+            Color::from(Hsl::new(118.0, 0.82, 0.50)).into()
+        )))?;
+
+    Ok(())
 }

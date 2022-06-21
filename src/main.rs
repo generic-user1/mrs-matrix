5e43f88c307ad0fa4c02fab4cd5fb44af7e7a909
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-06-21 15:57:36 -0400

    Add `SaturationDescending` and `HueVariation`
    Two new color algorithms

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use mrs_matrix::anim_loop;
 use mrs_matrix::raindrop::{charsets, color_algorithms};
 
+const DEFAULT_COLOR_ALGORITHM: color_algorithms::LightnessDescending =
+color_algorithms::LightnessDescending{
+    hue: 118.0,
+    saturation: 0.82
+};
+
 fn main() -> crossterm::Result<()> 
 {
     let charset = charsets::AsciiAndSymbols();
-    let color_algorithm = color_algorithms::LightnessDescending{
-        hue: 118.0,
-        saturation: 0.82
-    };
+    let color_algorithm = DEFAULT_COLOR_ALGORITHM;
     anim_loop(charset, color_algorithm, 0.75, 25)
 }
--- src/raindrop/color_algorithms.rs
+++ src/raindrop/color_algorithms.rs
@@ -55,3 +55,80 @@ impl ColorAlgorithm for LightnessDescending {
     }
 
 }
+
+/// Colors characters with varying saturation according to their distance from the leader
+/// 
+/// `hue` is the hue degree of the base color. It must be within the range `(0.0, 360.0]`.
+/// 
+/// `lightness` is the lightness amount of the base color. It must be within the range `(0.0, 1.0)`.
+/// 
+///# Notes
+/// 
+/// If `hue` or `lightness` are outside of their expected ranges, `gen_color` will panic
+#[derive(Clone, Copy)]
+pub struct SaturationDescending{
+    pub hue: f32,
+    pub lightness: f32
+}
+impl ColorAlgorithm for SaturationDescending {
+    fn gen_color(&self, raindrop: &Raindrop<Self>, position_in_follower: usize) -> Color {
+        assert!(self.hue >= 0.0 && self.hue < 360.0, "hue outside of expected bounds (0, 360]");
+        assert!(self.lightness >= 0.0 && self.lightness <= 1.0, 
+            "lightness outside of expected bounds (0, 1)");
+
+            //determine color saturation by subtracting the proportion
+            //of the char's position within the raindrop from 1.0; this results in follower chars
+            //decreasing in saturation as their distance from the leader increases
+            let position_in_follower = position_in_follower as f32;
+            let follower_length: f32 = raindrop.follower_content.len() as f32;
+
+            let follower_proportion = position_in_follower/follower_length;
+            
+            coolor::Color::Hsl(
+                Hsl{     
+                    h:self.hue, 
+                    l:self.lightness,
+                    //use of max ensures saturation is always 0.0 or above 
+                    s:((1.0 - follower_proportion).max(0.0))
+                }
+            )
+    }
+}
+
+/// Colors characters with varying hue according to their distance from the leader
+/// 
+/// `saturation` is the saturation amount of the base color. It must be within the range `(0.0, 1.0)`.
+/// 
+/// `lightness` is the lightness amount of the base color. It must be within the range `(0.0, 1.0)`.
+///
+///# Notes
+///  
+/// If `hue` or `lightness` are outside of their expected ranges, `gen_color` will panic
+#[derive(Clone, Copy)]
+pub struct HueVariation {
+    pub saturation: f32,
+    pub lightness: f32
+}
+impl ColorAlgorithm for HueVariation {
+    fn gen_color(&self, raindrop: &Raindrop<Self>, position_in_follower: usize) -> Color {
+        assert!(self.saturation >= 0.0 && self.saturation <= 1.0, 
+            "saturation outside of expected bounds (0, 1)");
+        assert!(self.lightness >= 0.0 && self.lightness <= 1.0, 
+            "lightness outside of expected bounds (0, 1)");
+
+            //determine color hue by multiplying follower proportion by 360,
+            //producing a valid hue value unique for each char position
+            let position_in_follower = position_in_follower as f32;
+            let follower_length: f32 = raindrop.follower_content.len() as f32;
+
+            let follower_proportion = position_in_follower/follower_length;
+            
+            coolor::Color::Hsl(
+                Hsl{     
+                    h:follower_proportion * 360.0,
+                    s:self.saturation, 
+                    l:self.lightness
+                }
+            )
+    }
+}

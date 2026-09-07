407a66bf422818fddf38e53de30dad20dacef0ae
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2026-09-07 18:34:20 -0400

    Resolve clippy warnings in `raindrop.rs`

--- src/raindrop.rs
+++ src/raindrop.rs
@@ -223,14 +223,11 @@ where
                     "Failed to represent follower_index ({}) as a usize; skipping char",
                     (self.row_index - 1) - provided_row_index
                 );
-                return None;
+                None
             }
             Ok(follower_index) => {
                 //return either the char at the follower index, or None if there isn't one
-                match self.follower_content.get(follower_index) {
-                    Some(&val) => Some(val),
-                    None => None
-                }
+                self.follower_content.get(follower_index).copied()
             }
         }
     }
@@ -259,7 +256,7 @@ where
                     let follower_length: f32 = self.follower_content.len() as f32;
 
                     let follower_proportion =
-                        (position_in_follower / follower_length).min(1.0).max(0.0);
+                        (position_in_follower / follower_length).clamp(0.0, 1.0);
 
                     let char_color = self.color_algorithm.gen_color(follower_proportion);
 
@@ -292,21 +289,19 @@ where
     ///
     /// This is similar to [move_drop](crate::raindrop::Raindrop::move_drop), with two key differences:
     /// - If the `Raindrop` is not visible because it has fallen down below the bottom of the terminal,
-    /// [reinit_state](crate::raindrop::Raindrop::reinit_state) is called to re-randomize the `Raindrop` and
-    /// move it slightly above the top of the terminal.
+    ///   [reinit_state](crate::raindrop::Raindrop::reinit_state) is called to re-randomize the `Raindrop` and
+    ///   move it slightly above the top of the terminal.
     ///
     /// - If the `Raindrop` has had its `advance_chance` set to some value that is not 1.0, this function
-    /// will only have a chance of advancing this raindrop's position. If you want to move the `Raindrop`
-    /// for certain, use the [move_drop](crate::raindrop::Raindrop::move_drop) method
+    ///   will only have a chance of advancing this raindrop's position. If you want to move the `Raindrop`
+    ///   for certain, use the [move_drop](crate::raindrop::Raindrop::move_drop) method
     pub fn advance_animation(&mut self, terminal_height: u16) {
         // only perform visibility check if current row is not less than 0
         // if we didn't make this check conditional, advance_animation would continuously call reinit_state
         // as raindrops always start above row 0 but are never visible until they reach row 0
-        if !(self.row_index < 0) {
-            if !self.is_visible(terminal_height) {
-                self.reinit_state(terminal_height);
-                return;
-            }
+        if !(self.row_index < 0) && !self.is_visible(terminal_height) {
+            self.reinit_state(terminal_height);
+            return;
         }
 
         if self.advance_chance == 1.0 {
